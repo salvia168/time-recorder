013327b72cc2d1383510ef6c70c860b4fb6bb5cf
yukiho11511 <ia15illust@gmail.com>
2023-07-01 00:06:07 +0900

    Initial Commit

---
+++ android/app/src/main/kotlin/jp/gr/java_conf/cotlra/time_recorder/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.gr.java_conf.cotlra.time_recorder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'pages/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blueGrey,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
